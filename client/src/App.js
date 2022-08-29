8a2ce467eb786370d785a0d33f795c6cc12a9f8a
Angela Gola <angelagola.ko@gmail.com>
2022-08-28 22:57:18 -0400

    Added user mutations, ability to add new users, navigate to user profiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 
 // const httpLink = createHttpLink({
@@ -19,8 +18,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
